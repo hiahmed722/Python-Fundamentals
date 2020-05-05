a10cbf59342169e7576e3df106f202e717062b3f
hiahmed722 <64818902+hiahmed722@users.noreply.github.com>
2020-05-04 21:51:15 -0400

    Add files via upload

---
+++ List data types.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+List data types
+
+
+# In[2]:
+
+
+students = ['vivek','pradeep','rashid','laxmi','jyotsna,'ashok','spandana']
+        
+print(students)
+
+
+# In[3]:
+
+
+type(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
