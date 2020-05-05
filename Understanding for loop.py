7119c36e9cd0950609a88e3542bc4946f74ad703
hiahmed722 <64818902+hiahmed722@users.noreply.github.com>
2020-05-05 19:33:19 -0400

    Add files via upload

---
+++ Understanding for loop.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Understanding for loops
+
+
+# In[1]:
+
+
+students = ['vasundhara','vivek','ravi','jyotsna','rahul','bikesh']
+
+
+# In[2]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+split out the students individually from a list
+
+
+# In[3]:
+
+
+for students in students:
+    print(students)
+
+
+# In[10]:
+
+
+General syntax of a for loop:
+    
+for tempvar in mainvar:
+    print(tempvar)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
